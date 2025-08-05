3b7c499d63f45e7241952e9ff2b2a0c6405030b4
EVOS-LP22-0007\User <mujiburrohman@noctua.gg>
2025-08-05 13:27:09 +0700

    Initial commit

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -115,7 +115,7 @@ publishing {
     repositories {
         maven {
             name = "GitLab"
-            url = uri("https://gitlab.com/api/v4/projects/72300320/packages/maven")
+            url = uri("https://gitlab.com/api/v4/projects/72374104/packages/maven")
             credentials(HttpHeaderCredentials::class) {
                 name = "Job-Token"
                 value = System.getenv("CI_JOB_TOKEN")
