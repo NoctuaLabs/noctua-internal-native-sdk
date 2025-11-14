d9928beca0c3bbde8d2bbb1f860297e7b1e6bfe5
EVOS-LP22-0007\User <mujiburrohman@noctua.gg>
2025-11-14 14:53:04 +0700

    feat: add session tags for TSPU tracking

--- sdk/src/commonMain/kotlin/utils/ExperimentManager.kt
+++ sdk/src/commonMain/kotlin/utils/ExperimentManager.kt
@@ -1,2 +1,46 @@
 package com.noctuagames.labs.sdk.utils
 
+internal object ExperimentManager {
+
+    private const val KEY_CURRENT_EXPERIMENT = "current_experiment"
+    private const val KEY_CURRENT_FEATURE = "current_feature"
+
+    private val experimentFlags = mutableMapOf<String, Any?>()
+
+    private fun <T> setFlag(key: String, value: T) {
+        experimentFlags[key] = value
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> getFlag(key: String, defaultValue: T): T {
+        return experimentFlags[key] as? T ?: defaultValue
+    }
+
+    fun clear() {
+        experimentFlags.clear()
+    }
+
+    fun setGeneralExperiment(key: String, value: String) {
+        setFlag(key, value)
+    }
+
+    fun getGeneralExperiment(key: String): String {
+        return getFlag(key, "")
+    }
+
+    fun setExperiment(experimentName: String) {
+        setFlag(KEY_CURRENT_EXPERIMENT, experimentName)
+    }
+
+    fun getExperiment(): String {
+        return getFlag(KEY_CURRENT_EXPERIMENT, "")
+    }
+
+    fun setSessionTag(featureName: String) {
+        setFlag(KEY_CURRENT_FEATURE, featureName)
+    }
+
+    fun getSessionTag(): String {
+        return getFlag(KEY_CURRENT_FEATURE, "")
+    }
+}
